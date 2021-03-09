e48510afaf662fba2b1f385460536ada5aa5840c
Davron Raximjanov <6795763-panterra-000@users.noreply.gitlab.com>
2021-03-09 20:38:42 +0500

    dddddawd

---
+++ app/src/main/java/uz/rdo/projects/xabarchichat/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.rdo.projects.xabarchichat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
