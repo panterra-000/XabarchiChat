3d64e6afdd2abcfce548c3fb3031396f8439aa4d
Davron Raximjanov <6795763-panterra-000@users.noreply.gitlab.com>
2021-03-10 18:09:33 +0500

    awdaw

--- app/src/main/java/uz/rdo/projects/xabarchichat/ui/screen/mainFragments/chatsPersonal/PersonalChatsFragment.kt
+++ app/src/main/java/uz/rdo/projects/xabarchichat/ui/screen/mainFragments/chatsPersonal/PersonalChatsFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import uz.rdo.projects.xabarchichat.databinding.FragmentPersonalChatsBinding
+import uz.rdo.projects.xabarchichat.ui.adapters.recycler.ChatAdapter
+import uz.rdo.projects.xabarchichat.ui.adapters.recycler.ContactsAdapter
 
 class PersonalChatsFragment : Fragment() {
 
     lateinit var binding: FragmentPersonalChatsBinding
+    lateinit var adapter: ChatAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,19 +26,17 @@ class PersonalChatsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         loadViews()
         loadObservers()
-
     }
 
     private fun loadObservers() {
 
-
     }
 
     private fun loadViews() {
-
-
-
+        adapter = ChatAdapter()
+        adapter.submitContacts(listOf())
+        binding.rvChat.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvChat.adapter = adapter
     }
-
 
 }
